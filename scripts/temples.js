0e3c5088e2b28c065c3c144d420f5383adaa2828
alexsantiago2023 <alexsantiago2003.2@gmail.com>
2024-09-28 15:16:26 -0600

    temples finish

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,11 @@
 document.getElementById("currentyear").innerHTML = new Date().getFullYear();
 
 document.getElementById("lastmodified").textContent = new Date(document.lastModified);
+
+const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+  mainnav.classList.toggle('show');
+  hambutton.classList.toggle('show');
+});
