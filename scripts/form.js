13f4d7096163d5537438209dfb528aa5a8334e3b
alexsantiago2023 <alexsantiago2003.2@gmail.com>
2024-10-16 23:34:46 -0600

    save

--- scripts/form.js
+++ scripts/form.js
@@ -1,30 +1,30 @@
 const products = [
     {
-      id: "fc-1888",
-      name: "flux capacitor",
-      averagerating: 4.5
+        id: "fc-1888",
+        name: "flux capacitor",
+        averagerating: 4.5
     },
     {
-      id: "fc-2050",
-      name: "power laces",
-      averagerating: 4.7
+        id: "fc-2050",
+        name: "power laces",
+        averagerating: 4.7
     },
     {
-      id: "fs-1987",
-      name: "time circuits",
-      averagerating: 3.5
+        id: "fs-1987",
+        name: "time circuits",
+        averagerating: 3.5
     },
     {
-      id: "ac-2000",
-      name: "low voltage reactor",
-      averagerating: 3.9
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averagerating: 3.9
     },
     {
-      id: "jj-1969",
-      name: "warp equalizer",
-      averagerating: 5.0
+        id: "jj-1969",
+        name: "warp equalizer",
+        averagerating: 5.0
     }
-  ];
+];
 
 function displayProducts(data) {
     const selectElement = document.querySelector("#product")
@@ -35,7 +35,8 @@ function displayProducts(data) {
         name.value = product.name;
 
         selectElement.appendChild(name)
-    })}
+    })
+}
 
 displayProducts(products)
 
